54e2e6d9b3662bc0d65d6123240b4168370709fd
ateyapayo <95537324+ateyapayo@users.noreply.github.com>
2022-02-10 19:48:59 +0100

    import browserroute

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
 
 <BrowserRouter basename={window.location.pathname || ''}>
   <Route exact path="/" component={Index} />
