eead4719a7b31b5bd3cd02150cd45bfa95475cd1
ateyapayo <95537324+ateyapayo@users.noreply.github.com>
2022-02-10 19:43:59 +0100

    added router

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
 
+<BrowserRouter basename={window.location.pathname || ''}>
+  <Route exact path="/" component={Index} />
+</BrowserRouter>
+
 ReactDOM.render(<App />, document.getElementById("root"))
